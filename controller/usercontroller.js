fcb26e816166e2af44e7644d66eadf46347e8648
c# <cdmi.development5@gmail.com>
2025-02-06 14:38:26 +0530

    msg

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -1,5 +1,7 @@
 var user = require('../model/usermodel');
 
+var login_status=0;
+
 exports.insert = async (req,res) => {
     user.create(req.body);
     res.status(200).json({
@@ -57,4 +59,40 @@ exports.delete_data = async (req,res) => {
     res.status(200).json({
         status:"success"
     })
+}
+
+exports.login = async (req,res) => {
+    var data = await user.find({"email":req.body.email});
+
+    if(login_status==0)
+    {
+        if(data.length==1)
+        {
+            if(data[0].password==req.body.password)
+            {
+                login_status=data[0].id;
+                res.status(200).json({
+                    status:"Login Success"
+                })
+            }
+            else
+            {
+                res.status(200).json({
+                    status:"check your email and password"
+                })
+            }
+        }
+        else
+        {
+            res.status(200).json({
+                status:"check your email and password"
+            })
+        }
+    }
+    else
+    {
+        res.status(200).json({
+            status:"user is already login"
+        })
+    }
 }
--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,11 @@ var user = require('../controller/usercontroller');
 
 
 router.post('/', user.insert);
-router.get('/', user.get_data);
+router.get('/get', user.get_data);
 router.get('/update/:id', user.get_update_data);
 router.post('/update/:id', user.update_data);
 router.get('/delete/:id', user.delete_data);
 
-
-
-
+router.post('/login', user.login);
 
 module.exports = router;
